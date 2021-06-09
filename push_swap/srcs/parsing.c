a3d23d9d960347a17ec9d67e1f00f072e282ebe7
nfauconn <nfauconn@student.42.fr>
2021-06-09 16:33:36 +0200

    libft

---
+++ push_swap/srcs/parsing.c
@@ -0,0 +1,14 @@
+#include "push_swap.h"
+
+int		main(int argc, char **argv)
+{
+	write(1, "ok\n", 3);
+/*	if (argc <= 1)
+		return (0);
+	if (argc == 2)
+	{
+		if (argv[1])
+			parse argv[1]
+	}
+*/
+}
